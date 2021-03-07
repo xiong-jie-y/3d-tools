057eed5a1bb3a7d088524b2f973337b46c0f6b43
xiong-jie-y <fwvillage@gmail.com>
2021-03-07 23:14:54 +0900

    map_conversion: Add intrinsic_3d.

--- map_conversion/convert_reconstructed_map.py
+++ map_conversion/convert_reconstructed_map.py
@@ -1,3 +1,4 @@
+"""This is the converter from open3d reconstructed map to some other formats."""
 import os
 import glob
 from os.path import join
@@ -5,8 +6,7 @@ import shutil
 import click
 import numpy as np
 import json
-
-from numpy.lib.utils import source
+from PIL import Image
 
 class SingleShotPoseGenerator:
     ":Broken"
@@ -44,9 +44,59 @@ class SingleShotPoseGenerator:
       }, open(os.path.join(destination_root_path, "intrinsics.json"), "w"))
 
 class Intrinsic3DDatasetGenerator:
-    pass
+    """Generates a dataset in the intrinsic 3d format.
 
-class DscastarGenerator: 
+    The format is described in https://vision.in.tum.de/data/datasets/intrinsic3d
+    and http://graphics.stanford.edu/projects/vsfs/.
+    """
+    def __init__(self, output_root):
+        self.output_root = join(output_root, "0")
+        os.makedirs(self.output_root, exist_ok=True)
+
+    def output_rgb_files(self, src_rgb_paths):
+        # This should be png.
+        for i, src_rgb_path in enumerate(src_rgb_paths):
+            im = Image.open(src_rgb_path)
+            im.save(join(self.output_root, "frame-{:06}.color.png".format(i)))
+    
+    def output_depth_files(self, src_depth_paths):
+        for i, src_depth_path in enumerate(src_depth_paths):
+            shutil.copy(
+                src_depth_path,
+                join(self.output_root, "frame-{:06}.depth.png".format(i))
+            )
+
+    def output_trajectory(self, pose_matrixes):
+        # output pose is just ordinary homogenious transformation.
+        for i, pose_matrix in enumerate(pose_matrixes):
+            lines = []
+            for row in pose_matrix:
+                # This need to end with \n.
+                # Line doesn't mean something that end with \n.
+                lines.append(" ".join(row.astype(np.str)) + "\n")
+            open(join(self.output_root, "frame-{:06}.pose.txt".format(i)), "w").writelines(lines)
+
+    def output_intrinsic(self, intrinsic):
+        # The output intrinsic should be 4x4 matrix.
+        # The example is
+        # 600 0 300 0
+        # 0 600 200 0
+        # 0 0   1   0
+        # 0 0   0  1
+        intrinsic = np.array(intrinsic["intrinsic_matrix"])
+        matrix = np.identity(4)
+        matrix[:3,:3] = intrinsic.reshape(3,3).transpose()
+        lines = []
+        for row in matrix:
+            lines.append(" ".join(row.astype(np.str)) + "\n")
+        open(join(self.output_root, "colorintrinsics.txt"), "w").writelines(lines)
+        open(join(self.output_root, "depthintrinsics.txt"), "w").writelines(lines)
+
+class DscastarGenerator:
+    """This class generate a data structure for DSACSTAR.
+
+    The format is https://github.com/vislearn/dsacstar#data-structure.
+    """
     def __init__(self, output_root):
         self.output_root = join(output_root, "train")
         self.seq = 0
@@ -86,6 +136,7 @@ class DscastarGenerator:
         calibration_folder = join(self.output_root, 'calibration')
         os.makedirs(calibration_folder, exist_ok=True)
         assert (self.frame_length)
+        # This is wierd but this dataset only support fx.
         for i in range(0, self.frame_length):
             open(join(calibration_folder, "{}-frame-{:06}.calibration.txt".format(self.seq, i)), "w") \
                 .write(str(intrinsic["intrinsic_matrix"][0]))
@@ -100,6 +151,8 @@ def main(source_path, destination_path, to_type):
     generator = None
     if to_type == "dsacstar":
         generator = DscastarGenerator(destination_root_path)
+    elif to_type == "intrinsic_3d":
+        generator = Intrinsic3DDatasetGenerator(destination_root_path)
     else:
         raise RuntimeError(f"no to_type = {to_type}.")
 
@@ -129,8 +182,6 @@ def main(source_path, destination_path, to_type):
             [float(t) for t in lines[i + 3].split(" ")],
             [float(t) for t in lines[i + 4].split(" ")]
         ]
-        print(rows)
-        # mats.append(np.linalg.inv(np.array(rows)))
         mats.append(np.array(rows))
     generator.output_trajectory(mats)    
 
